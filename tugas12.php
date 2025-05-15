593fc3e58ed4cb383caebad74104346fe1930259
fanzila-cyber <zilayy.sch>
2025-05-15 15:35:00 +0800

    tembahan

---
+++ tugas12.php
@@ -0,0 +1,21 @@
+<?php
+ $anggota_kelas = [
+    "Akifah",
+    "Alifah",
+    "Adawiyah",
+    "Fahrah",
+    "Fanzila",
+    "Fauziyyah",
+    "Firqah",
+    "Hera",
+    "Jilan",
+    "Khayla",
+    "Muftihah",
+    "Nadia",
+    "Putri",
+    "Zahra",
+ ];
+echo "Anggota Kelas 10-B </br></br>";
+    foreach ($anggota_kelas as $kelas10){
+        echo  "-  " .$kelas10 ."</br>";
+}
